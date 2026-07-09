b8ed7b9822dd8c3491347f21bc3c1677c39fc3cb
leosaq <leandrosaquisari7@gmail.com>
2026-07-09 12:01:33 -0500

    Cambios1

--- taller/taller13/inmuebles/admin.py
+++ taller/taller13/inmuebles/admin.py
@@ -4,12 +4,12 @@ from .models import Edificio, Departamento
 
 class EdificioAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'direccion', 'ciudad', 'tipo')
-    search_fields = ('nombre', 'ciudad')
-    list_filter = ('tipo',)
+    search_fields = ("nombre", "direccion", "ciudad")
+    list_filter = ("tipo", "ciudad")
 admin.site.register(Edificio, EdificioAdmin)
 
 class DepartamentoAdmin(admin.ModelAdmin):
     list_display = ('nombre_completo_propietario', 'costo_departamento', 'numero_cuartos', 'edificio')
-    search_fields = ('nombre_completo_propietario', 'edificio__nombre')
+    search_fields = ('nombre_completo_propietario', 'edificio__nombre', 'edificio__ciudad')
     list_filter = ('edificio',)
 admin.site.register(Departamento, DepartamentoAdmin)
--- taller/taller13/inmuebles/models.py
+++ taller/taller13/inmuebles/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Edificio(models.Model):
     TIPO_CHOICES = [
-        ('residencial', 'Residencial'),
-        ('comercial', 'Comercial'),
+        ("residencial", "Residencial"),
+        ("comercial", "Comercial"),
     ]
 
     nombre = models.CharField(max_length=100)
@@ -23,8 +23,10 @@ class Departamento(models.Model):
     edificio = models.ForeignKey(
         Edificio,
         on_delete=models.CASCADE,
-        related_name='departamentos'
+        related_name="departamentos"
     )
 
     def __str__(self):
-        return "%s - %s" % (self.nombre_completo_propietario, self.edificio.nombre)
+        return "%s - %s" % (self.nombre_completo_propietario,
+            self.edificio.nombre
+        )
