6bc35f4bf0b2b6f1c57f823d1e233883eec3b0e7
leosaq <leandrosaquisari7@gmail.com>
2026-07-09 11:32:47 -0500

    Cambios1

--- taller/taller13/inmuebles/admin.py
+++ taller/taller13/inmuebles/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Edificio, Departamento
+
+class EdificioAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'direccion', 'ciudad', 'tipo')
+    search_fields = ('nombre', 'ciudad')
+    list_filter = ('tipo',)
+admin.site.register(Edificio, EdificioAdmin)
+
+class DepartamentoAdmin(admin.ModelAdmin):
+    list_display = ('nombre_completo_propietario', 'costo_departamento', 'numero_cuartos', 'edificio')
+    search_fields = ('nombre_completo_propietario', 'edificio__nombre')
+    list_filter = ('edificio',)
+admin.site.register(Departamento, DepartamentoAdmin)
---
+++ taller/taller13/inmuebles/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from inmuebles.models import Edificio, Departamento
+from rest_framework import serializers
+
+class EdificioSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Edificio
+        fields = '__all__'
+
+
+class DepartamentoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Departamento
+        fields = '__all__'
